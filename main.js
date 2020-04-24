6ba6cf465374321a7468257f7f8033d2c4973721
PilarCha <pchavez92@gmail.com>
2020-04-23 20:37:34 -0700

    Added accelarator to  keyboard quit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -10,11 +10,45 @@ let mainWindow;
 
 app.on('ready', function() {
   //create new window
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   //load HTML into window
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
     slashes: true
   }));
+  //build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //insert menu
+  Menu.setApplicationMenu(mainMenu);
+
 });
+
+//create menu tamplate
+
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item'
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label:'Quit',
+        //shortcut for ctrl q. testing is its a mac or win/linux
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl + Q',
+        click() {
+          app.quit();
+        }
+      }
+
+  ]
+  }
+]
