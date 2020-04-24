8bad19b7dab8d79a978e63f366640d40e02d3af2
PilarCha <pchavez92@gmail.com>
2020-04-23 21:14:55 -0700

    Add window has been added. App quit and garbage collector when window is closed

--- main.js
+++ main.js
@@ -5,9 +5,8 @@ const path = require('path');
 const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
-
+let addWindow;
 //Listen for app to be ready
-
 app.on('ready', function() {
   //create new window
   mainWindow = new BrowserWindow({
@@ -21,6 +20,10 @@ app.on('ready', function() {
     protocol: 'file:',
     slashes: true
   }));
+  //Quit app when closed
+  mainWindow.on('closed', () => {
+    app.quit();
+  })
   //build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   //insert menu
@@ -28,6 +31,29 @@ app.on('ready', function() {
 
 });
 
+//createAddWindow
+createAddWindow = () => {
+  //create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add Shopping List Item',
+
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  //load HTML into window
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+  // Garbage collection handle
+  addWindow.on('close', () => {
+    addWindow = null;
+  })
+}
 //create menu tamplate
 
 const mainMenuTemplate = [
@@ -35,7 +61,10 @@ const mainMenuTemplate = [
     label: 'File',
     submenu: [
       {
-        label: 'Add Item'
+        label: 'Add Item',
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: 'Clear Items'
@@ -44,6 +73,7 @@ const mainMenuTemplate = [
         label:'Quit',
         //shortcut for ctrl q. testing is its a mac or win/linux
         accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl + Q',
+
         click() {
           app.quit();
         }
